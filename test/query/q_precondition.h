722f2e4bfb5fcea466966ba94561d5fece867be8
fah <farhan.abuhazan@gmail.com>
2019-08-13 19:41:30 +0700

    implement query q_precondition for timer and add test cases

--- test/query/q_precondition.h
+++ test/query/q_precondition.h
@@ -73,9 +73,29 @@ void q_precondition_no_match_type() {
   TEST_ASSERT_TRUE(resJson["message"] == "No such the provided type.");
 }
 
+void q_precondition_with_timer_param_check_correct_type() {
+  StaticJsonDocument<64> data;
+  data["type"] = "timer";
+  data["index"] = 0;
+  JsonTopic topic("precondition", "query", data.as<JsonObject>());
+  String result = resolvers.execute(topic.toStaticJsonObject().as<JsonObject>());
+
+  StaticJsonDocument<512> resJson;
+  DeserializationError error = deserializeJson(resJson, result);
+  if (error) {
+    TEST_ASSERT_FALSE(error);
+  }
+
+  TEST_ASSERT_TRUE(resJson["topic"] == "precondition");
+  TEST_ASSERT_TRUE(resJson["method"] == "query");
+  TEST_ASSERT_TRUE(resJson["data"]["index"] == 0);
+  TEST_ASSERT_TRUE(!resJson["data"]["timers"].isNull());
+}
+
 void q_precondition_RUN_TEST() {
   RUN_TEST(q_precondition_type_is_not_defined);
   RUN_TEST(q_precondition_index_is_not_defined);
   RUN_TEST(q_precondition_index_out_of_range);
   RUN_TEST(q_precondition_no_match_type);
+  RUN_TEST(q_precondition_with_timer_param_check_correct_type);
 }
