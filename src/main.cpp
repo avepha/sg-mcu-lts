9fee2bcc6255273dbe773310565cb1a7a5ec006a
fah <farhan.abuhazan@gmail.com>
2019-09-18 17:24:26 +0700

    add vpd to sg_test mode

--- src/main.cpp
+++ src/main.cpp
@@ -104,7 +104,7 @@ void loop1(void *pvParameters) {
 void loop() {
 #ifdef SG_TEST
   for (int i = 1 ; i < 3; i++) { // mock 3 stations
-    int bucketSize = 6;
+    int bucketSize = 7;
     uint16_t sensorBucket[bucketSize];
     sensorBucket[0] = Float16Compressor::compress((float) random(280, 288) / 10);
     sensorBucket[1] = Float16Compressor::compress((float) random(500, 510) / 10);
@@ -112,6 +112,28 @@ void loop() {
     sensorBucket[3] = Float16Compressor::compress((float) random(510, 520) / 10);
     sensorBucket[4] = Float16Compressor::compress((float) random(10, 15) / 10);
     sensorBucket[5] = Float16Compressor::compress((float) random(10, 15) / 10);
+    sensorBucket[6] = Float16Compressor::compress((float) random(10, 15) / 10);
+
+    byte payloadSize = bucketSize * sizeof(uint16_t) + sizeof(byte) * 4;
+    byte payload_sta_1[payloadSize];
+    craftBytesArrayOfSensorPayload(i, sensorBucket, bucketSize, payload_sta_1);
+
+    int plain_payload_size = payloadSize - 3;
+    byte plain_payload[plain_payload_size];
+    memcpy(plain_payload, payload_sta_1 + 1, plain_payload_size);
+
+    context->nSensorContext->core->updateNSensor(plain_payload, plain_payload_size);
+  }
+  for (int i = 1 ; i < 3; i++) { // mock 3 stations
+    int bucketSize = 7;
+    uint16_t sensorBucket[bucketSize];
+    sensorBucket[0] = Float16Compressor::compress((float) random(280, 288) / 10);
+    sensorBucket[1] = Float16Compressor::compress((float) random(500, 510) / 10);
+    sensorBucket[2] = Float16Compressor::compress((float) random(31000, 31500) / 10);
+    sensorBucket[3] = Float16Compressor::compress((float) random(290, 298) / 10);
+    sensorBucket[4] = Float16Compressor::compress((float) random(510, 520) / 10);
+    sensorBucket[5] = Float16Compressor::compress((float) random(10, 15) / 10);
+    sensorBucket[6] = Float16Compressor::compress((float) random(10, 15) / 10);
 
     byte payloadSize = bucketSize * sizeof(uint16_t) + sizeof(byte) * 4;
     byte payload_sta_1[payloadSize];
