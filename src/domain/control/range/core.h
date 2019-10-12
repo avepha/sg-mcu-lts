c6cf6dd1baaacce23ba76da8280bcec531f075b5
fah <farhan.abuhazan@gmail.com>
2019-10-12 14:28:33 +0700

    implement range logic.

--- src/domain/control/range/core.h
+++ src/domain/control/range/core.h
@@ -9,8 +9,27 @@
 
 class RangeState : public State {
 public:
+  enum RANGE_STATE_ENUM {
+    RANGE_STATE_WAITING = 0,
+    RANGE_STATE_WORKING = 1
+  };
+  bool isTimingEnable = false;
+  RANGE_STATE_ENUM rangeState = RANGE_STATE_WAITING;
+  float sensorValue = 0.0F;
+  bool isReachThreshold = false;
+  unsigned long currentWaitingTimeInSecond = 0;
+  unsigned long currentWorkingTimeInSecond = 0;
+
   JsonDocument report() {
     DynamicJsonDocument data(64);
+    data["type"] = "range";
+    data["sensorValue"] = sensorValue;
+    data["isReachThreshold"] = isReachThreshold;
+    if (isTimingEnable) {
+      data["rangeState"] = (rangeState == RANGE_STATE_WAITING) ? "waiting" : "working";
+      data["currentWaitingTimeInSecond"] = currentWaitingTimeInSecond;
+      data["currentWorkingTimeInSecond"] = currentWorkingTimeInSecond;
+    }
     return data;
   }
 };
@@ -33,6 +52,50 @@ public:
   }
 
   bool controlTask() override {
+    NSensor sensor = nSensorCore->getAverageSensor();
+    state.sensorValue = sensor.sensors[range.sensor];
+
+    state.isReachThreshold = (range.greater) ? state.sensorValue >= range.lower && state.sensorValue >= range.upper
+                                             : state.sensorValue <= range.lower && state.sensorValue <= range.upper;
+
+    if (state.isTimingEnable) {
+      switch (state.rangeState) {
+        case RangeState::RANGE_STATE_WAITING : {
+          state.currentWaitingTimeInSecond = (millis() - timeStamp) / 1000;
+          if (state.currentWaitingTimeInSecond < range.timing.waitingTimeInSecond) {
+            return true;
+          }
+
+          if (state.isReachThreshold) {
+            // go to working state
+            dWrite(channel, HIGH);
+            state.rangeState = RangeState::RANGE_STATE_WORKING;
+          }
+
+          timeStamp = millis();
+        }
+        case RangeState::RANGE_STATE_WORKING : {
+          state.currentWorkingTimeInSecond = (millis() - timeStamp) / 1000;
+          if (state.currentWorkingTimeInSecond < range.timing.workingTimeInSecond) {
+            return true;
+          }
+
+          bool shouldStopWorking = (range.greater) ? state.sensorValue >= range.lower
+                                                   : state.sensorValue <= range.upper;
+          if (!shouldStopWorking) {
+            return true;
+          }
+
+          dWrite(channel, LOW);
+          timeStamp = millis();
+          state.rangeState = RangeState::RANGE_STATE_WAITING;
+        }
+      }
+    }
+    else {
+      state.isReachThreshold ? dWrite(channel, HIGH) : dWrite(channel, LOW);
+    }
+
     return true;
   }
 
