5481dd256cb76d6fad515413a099cfbb669e8fc5
fah <farhan.abuhazan@gmail.com>
2019-10-13 11:58:31 +0700

    bump version

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include <TaskScheduler.h>
 #include <ArduinoJson.h>
 
-#define VERSION "0.2.0"
+#define VERSION "0.2.1"
 #define PROJECT "SMART-FARM"
 #define UPLOADDATE String(__DATE__) + " " + String(__TIME__)
 #define COMCORE 0
