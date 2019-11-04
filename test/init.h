a5a3f4a00d51ff247ea0bbbaa1b990d5ff95d9a3
fah <farhan.abuhazan@gmail.com>
2019-11-05 03:28:42 +0700

    refactor unit test

--- test/init.h
+++ test/init.h
@@ -1,21 +1,34 @@
 #ifndef SG_MCU_INIT_H
 #define SG_MCU_INIT_H
 #define _TASK_OO_CALLBACKS
+#define _TASK_PRIORITY
 
-#define VERSION "0.0.1"
+#include <unity.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <EEPROM.h>
+#include "config.h"
+#include <TaskScheduler.h>
+#include <ArduinoJson.h>
+
+#define VERSION "0.2.4"
+#define PROJECT "SMART-FARM"
+#define UPLOADDATE String(__DATE__) + " " + String(__TIME__)
+#define COMCORE 0
+#define MAINCORE 1
 #define EEPROM_SIZE 4096
 
-#include <Arduino.h>
-#include <ArduinoJson.h>
-#include <Unity.h>
-#include <RTClib.h>
+Scheduler controlScheduler, gpioScheduler;
 
-#include "../src/config.h"
-#include "util/JsonRequest.h"
-#include "../src/combineContext.h"
-#include "../src/combineResolvers.h"
+#include "util/util.h"
+#include "combineContext.h"
+#include "combineResolvers.h"
 
-CombineContext *context;
+#include "validationError.h"
+#include "deviceEndpoint.h"
+#include "sensorEndpoint.h"
+
 CombineResolvers *resolvers;
+CombineContext *context;
 
 #endif //SG_MCU_INIT_H
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,16 +1,12 @@
 #include "init.h"
 #include "mutation/m_date_save.h"
 #include "mutation/m_sensor_order_save.h"
-#include "mutation/m_precondition_save.h"
-#include "mutation/m_criteria.h"
 #include "mutation/m_timer_save.h"
 #include "mutation/m_channel_save.h"
 
 #include "query/q_date.h"
 #include "query/q_sensor.h"
 #include "query/q_sensor_order.h"
-#include "query/q_precondition.h"
-#include "query/q_criteria.h"
 #include "query/q_timer.h"
 #include "query/q_nsensor.h"
 #include "query/q_nsensors.h"
@@ -25,8 +21,9 @@ void setup() {
   Serial.begin(115200);
   EEPROM.begin(EEPROM_SIZE);
 
-  context = new CombineContext;
+  context = new CombineContext();
   resolvers = new CombineResolvers(context);
+
   delay(1000);
 
   UNITY_BEGIN();
@@ -34,8 +31,6 @@ void setup() {
   q_date_RUN_TEST();
   q_sensor_RUN_TEST();
   q_sensor_order_RUN_TEST();
-  q_precondition_RUN_TEST();
-  q_criteria_RUN_TEST();
   q_timer_RUN_TEST();
   q_nsensor_RUN_TEST();
   q_nsensors_RUN_TEST();
@@ -43,8 +38,6 @@ void setup() {
 
   m_date_save_RUN_TEST();
   m_sensor_order_save_RUN_TEST();
-  m_precondition_save_RUN_TEST();
-  m_criteria_save_RUN_TEST();
   m_timer_save_RUN_TEST();
   new t_channel_save();
 
