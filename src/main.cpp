86e8a63854ba7df750e38c494e51c60db805ad74
fah <farhan.abuhazan@gmail.com>
2019-11-13 04:36:50 +0700

    bump version to 1.1.4

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 #include <TaskScheduler.h>
 #include <ArduinoJson.h>
 
-#define VERSION "1.1.3"
+#define VERSION "1.1.4"
 #define PROJECT "SMART-FARM"
 #define UPLOADDATE String(__DATE__) + " " + String(__TIME__)
 #define COMCORE 0
