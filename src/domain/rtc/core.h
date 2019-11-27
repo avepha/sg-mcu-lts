bbf93374afd1dc37555995bec71595f395b81558
fah <farhan.abuhazan@gmail.com>
2019-11-27 21:36:44 +0700

    fix bug, check rtc status

--- src/domain/rtc/core.h
+++ src/domain/rtc/core.h
@@ -2,22 +2,28 @@
 #include <RTClib.h>
 #include "./model.h"
 
+#define RTC_ADDR 0x68
+
 #ifndef SG_MCU_RTC_CORE_H
 #define SG_MCU_RTC_CORE_H
 
 class RtcCore {
 public:
-  static RtcCore* instance()
-  {
+  static RtcCore *instance() {
     if (!s_instance)
       s_instance = new RtcCore;
     return s_instance;
   }
 
   RtcCore();
+
   DateTime getDate();
+
   DateTime getUtcDate();
+
   DateTime setDate(DateTime dt);
+
+  bool isRtcRunning();
 
 private:
   static RtcCore *s_instance;
@@ -32,21 +38,29 @@ RtcCore::RtcCore() {
   hwRtc.begin();
   swRtc.adjust(DateTime(2019, 01, 01));
   rtcModel = new RtcModel;
-
-  Debug::Print("RTC status: " + String(hwRtc.isrunning()));
+  if (isRtcRunning()) {
+    Debug::Print("RTC status: running");
+  }
+  else {
+    Debug::Print("RTC status: not running, instantiate softRtc...");
+  }
 }
 
 DateTime RtcCore::getDate() {
-  RtcSchema rtcSchema =  rtcModel->get();
-  DateTime utcDt = hwRtc.isrunning() ? hwRtc.now() : swRtc.now();
+  RtcSchema rtcSchema = rtcModel->get();
+  DateTime utcDt = isRtcRunning() ? hwRtc.now() : swRtc.now();
   TimeSpan tzTimeSpan(rtcSchema.tzOffsetHour * 3600 + rtcSchema.tzOffsetMin * 30);
   return utcDt + tzTimeSpan;
 }
 
 DateTime RtcCore::getUtcDate() {
-  return hwRtc.isrunning() ? hwRtc.now() : swRtc.now();
+  return isRtcRunning() ? hwRtc.now() : swRtc.now();
 }
 
+bool RtcCore::isRtcRunning() {
+  Wire.beginTransmission(RTC_ADDR);
+  return (Wire.endTransmission() == 0);
+}
 
 DateTime RtcCore::setDate(DateTime dt) {
   hwRtc.adjust(dt);
